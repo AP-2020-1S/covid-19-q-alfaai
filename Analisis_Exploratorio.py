93338993e9af213a15ac3aa55f1adbda71b4d3f1
mcirop <68129448+mcirop@users.noreply.github.com>
2020-08-17 21:33:30 -0500

    Update Analisis_Exploratorio.py

--- Analisis_Exploratorio.py
+++ Analisis_Exploratorio.py
@@ -7,3 +7,5 @@ from sodapy import Socrata
 client = Socrata("www.datos.gov.co", None)
 results = client.get("gt2j-8ykr", limit=1000000)
 results_df = pd.DataFrame.from_records(results)
+
+results_df.describe()
